4064626cd78eb140e9e9c8d5996a8558a908d911
Corey Frang <gnarf@gnarf.net>
2012-10-08 16:29:09 -0500

    Upping worker processes to 8 to test something

--- manifests/params.pp
+++ manifests/params.pp
@@ -18,7 +18,7 @@ class nginx::params {
   $nx_run_dir  = '/var/nginx'
 
   $nx_conf_dir           = '/etc/nginx'
-  $nx_worker_processes   = 4
+  $nx_worker_processes   = 8
   $nx_worker_connections = 1024
   $nx_multi_accept       = off
   $nx_sendfile           = on
